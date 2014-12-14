630d3d9cc5600352f198f2aa9b8c7f5f1e7717df
jh69 <jh69@users.noreply.github.com>
2014-12-13 19:43:55 -0800

    Update caltrain.py

--- caltrain.py
+++ caltrain.py
@@ -2,16 +2,8 @@
 
 import sys, csv, datetime, time
 
-if len(sys.argv) == 1:
-	print "specify a destination number. btw holiday schedules are not supported bc im lazy" \
-	"\n01 = sf, 02 = 22nd street, 03 = bayshore" \
-	"\n04 = south sf, 05 = san bruno, 06 = millbrae, 07 = broadway, 08 = burlingame" \
-	"\n09 = san mateo, 10 = hayward park, 11 = hillsdale, 12 = belmont, 13 = san carlos" \
-	"\n14 = redwood city, 15 = atherton, 16 = menlo park, 17 = palo alto, 19 = cal ave" \
-	"\n20 = san antonio, 21 = mountain view, 22 = sunnyvale, 23 = lawrence" \
-	"\n24 = santa clara, 25 = college park, 26 = san jose, 27 = tamien, 28 = capitol" \
-	"\n29 = blossom hill, 30 = morgan hill, 31 = san martin, 32 = gilroy"
-	exit()
+
+
 
 if datetime.datetime.today().weekday() > 5:
 	dayofweek = "Weekday"
@@ -20,17 +12,31 @@ elif datetime.datetime.today().weekday() == 5:
 elif datetime.datetime.today().weekday() == 6:
 	dayofweek = "Sunday"
 
-currtime = time.strftime("%H:%M:00")
-homestop = "21" #mv
-destination = sys.argv[1]
-stops = []
-todaysstops = []
-todaysstopsdeparting = []
-todaysstopsboth = []
-listoftrips = []
-stopsatdestination = []
-sortedlist = []
-bounddir = ""
+def addprecedingzero(stop):
+	if len(stop) == 1:
+		return "0" + stop
+	else:
+		return stop
+		
+if len(sys.argv) == 1:
+	print "specify destinations. first is departing station, second is destination." \
+	"\nif you only specify 1 it'll be your destination and home will be mountain view." \
+	"\nbtw holiday schedules are not supported bc im lazy" \
+	"\n01 = sf, 02 = 22nd street, 03 = bayshore" \
+	"\n04 = south sf, 05 = san bruno, 06 = millbrae, 07 = broadway, 08 = burlingame" \
+	"\n09 = san mateo, 10 = hayward park, 11 = hillsdale, 12 = belmont, 13 = san carlos" \
+	"\n14 = redwood city, 15 = atherton, 16 = menlo park, 17 = palo alto, 19 = cal ave" \
+	"\n20 = san antonio, 21 = mountain view, 22 = sunnyvale, 23 = lawrence" \
+	"\n24 = santa clara, 25 = college park, 26 = san jose, 27 = tamien, 28 = capitol" \
+	"\n29 = blossom hill, 30 = morgan hill, 31 = san martin, 32 = gilroy"
+	exit()
+elif len(sys.argv) == 2:
+	destination = addprecedingzero(sys.argv[1])
+	homestop = "21" #mv
+elif len(sys.argv) == 3:
+	
+	destination = addprecedingzero(sys.argv[2])
+	homestop = addprecedingzero(sys.argv[1])
 
 if int(destination) > int(homestop):
 	bounddir = "2"
@@ -40,9 +46,19 @@ else:
 	print "some shit is fucked with your destinations. try again."
 	exit()
 	
+currtime = time.strftime("%H:%M:00") 
+stops = []
+todaysstops = []
+todaysstopsdeparting = []
+todaysstopsboth = []
+listoftrips = []
+stopsatdestination = []
+sortedlist = []
+bounddir = ""
+
 
 print "It is", currtime[:5], "on a", dayofweek + \
-". Next trains leaving for", destination, "are at:"
+". Next trains leaving from", homestop, "for", destination, "are at:"
 
 # get the whole damn timetable file
 
